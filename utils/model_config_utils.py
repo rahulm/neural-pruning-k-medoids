ca1e7595876fad6c07fe039254082769eb81c87e
Rahul Malavalli <rahul.malavalli@gmail.com>
2020-11-15 22:56:02 -0800

    Added model config, added it to prune config

--- utils/model_config_utils.py
+++ utils/model_config_utils.py
@@ -1,46 +1,39 @@
 """
-This is a utils file for reading of pruning config JSON files.
+This is a utils file for reading of model config JSON files.
 """
 
 from typing import Dict, Text, Tuple
 
-from . import model_config_utils
 
-
-class PruneConfig:
+class ModelConfig:
     """
     Wrapper for easy access of config fields
     """
 
     _raw_dict: Dict
     config_id: Text
-    model_config: model_config_utils.ModelConfig
-    prune_type: Text
-    prune_params: Dict
-    pruned_model_out_folder: Text
+    model_architecture: Text
+    model_params: Dict
+    model_folder: Text
 
     def __init__(self, config_dict: Dict) -> None:
         self._raw_dict: Dict = config_dict
 
     def __getattr__(self, name):
         if name in self._raw_dict:
-            val = self._raw_dict[name]
-            if name == "model_config":
-                return model_config_utils.ModelConfig(val)
-            else:
-                return val
+            return self._raw_dict[name]
         return super().__getattribute__(name)
 
     def __repr__(self) -> Text:
         return str(self._raw_dict)
 
 
-def get_config_from_file(config_file_loc: Text) -> PruneConfig:
+def get_config_from_file(config_file_loc: Text) -> ModelConfig:
     import json
 
-    config: PruneConfig
+    config: ModelConfig
     with open(config_file_loc, "r") as config_file:
         config_dict: Dict = json.load(config_file)
-        config = PruneConfig(config_dict)
+        config = ModelConfig(config_dict)
     return config
 
